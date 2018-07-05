8dc8249278e3074121fd8cf7985466a8e85effb5
mitgobla <ben-dodd@outlook.com>
2018-07-05 23:08:48 +0100

    Updated render-test.py
    Render-test.py will now render a completed version of the model, and display an animated version of it building the model (currently a coordinate at a time, no smoothing)

--- lego-print/create-coordinates.py
+++ lego-print/create-coordinates.py
@@ -16,31 +16,29 @@ Each position has 4 values:
         2: blue
         3: yellow
 """
+
+import os
+script_dir = os.path.dirname(os.path.realpath(__file__))
+
 model_coordinates = [
-    [0, 3, 1, 1], #Front of Box
-    [3, 3, 1, 1],
-    [5, 3 , 1, 1],
-    [7, 3 , 1 , 1],
-    [9, 3 , 1, 1],
-    [11, 3, 1, 1],  #Right Side of box
+    [1, 1, 1, 1], #Front of Box
+    [3, 1, 1, 1],
+    [5, 1, 1, 1],
+    [7, 1, 1 , 1],
+    [9, 1, 1, 1],
+    [11, 1, 1, 1],  #Right Side of box
+    [11, 3, 1, 1],
     [11, 5, 1, 1],
     [11, 7, 1, 1],
     [11, 9, 1, 1],
-    [11, 11, 1, 1],
-    [9, 11, 1, 1], #Top Side of box
-    [7, 11, 1, 1],
-    [5, 11, 1, 1],
-    [3, 11, 1, 1],
-    [0, 11, 1, 1], #Left side of box
-    [0, 9, 1, 1],
-    [0, 7, 1, 1],
-    [0, 5, 1, 1],
-    
-    #[3, 3, 2, 2],
-    #[3, 3, 3, 3],
-    #[3, 3, 4, 2],
-    #[3, 3, 5, 1],
-    #[3, 3, 7, 0]
+    [9, 9, 1, 1], #Top Side of box
+    [7, 9, 1, 1],
+    [5, 9, 1, 1],
+    [3, 9, 1, 1],
+    [1, 9, 1, 1], #Left side of box
+    [1, 7, 1, 1],
+    [1, 5, 1, 1],
+    [1, 3, 1, 1]
 ]
 
 coordinates = []
@@ -48,19 +46,18 @@ for pos in range(0, len(model_coordinates), 1):
     #write each number on a new line
     
     if model_coordinates[pos][3] == 1:
-        coordinates.append([-10, 1, 1.2, 0]) #Red Brick Coord
-        print("red")
+        coordinates.append([-4, 1, 1.2, 0]) #Red Brick Coord
     elif model_coordinates[pos][3] == 2:
-        coordinates.append([-10, 7, 1.2, 0]) #Blue Brick Coord
-        print("blue")
+        coordinates.append([-4, 7, 1.2, 0]) #Blue Brick Coord
     elif model_coordinates[pos][3] == 3:
-        coordinates.append([-10, 13, 1.2, 0]) #Yellow Brick Coord
-        print("yellow")
+        coordinates.append([-4, 13, 1.2, 0]) #Yellow Brick Coord
     coordinates.append(model_coordinates[pos])
 
 #coordinates.reverse()
-print(coordinates)
-model = open('./coordinates/model.rtf', 'w')
+coord_file = open(script_dir+'\\coordinates\\coord.rtf', 'w')
+coord_file.write(str(coordinates))
+coord_file.close()
+model = open(script_dir+'\\coordinates\\model.rtf', 'w')
 
 model.write("{:.4f}".format(len(coordinates))+chr(13))
 for axes in range(4):
--- lego-print/resources/render-test.py
+++ lego-print/resources/render-test.py
@@ -9,30 +9,48 @@ import os
 import numpy as np
 import pyqtgraph.opengl as gl
 from pyqtgraph.dockarea import *
-from pyqtgraph import mkColor
+from pyqtgraph import mkColor, Transform3D
 from pyqtgraph.Qt import QtCore, QtGui
 
 script_dir = os.path.dirname(os.path.realpath(__file__))
 
 model = [
-    [1, 3, 1, 1], #Front of Box
-    [3, 3, 1, 1],
-    [5, 3 , 1, 1],
-    [7, 3 , 1 , 1],
-    [9, 3 , 1, 1],
-    [11, 3, 1, 1],  #Right Side of box
+    [-4, 1, 1.2, 0], 
+    [1, 1, 1, 1], 
+    [-4, 1, 1.2, 0], 
+    [3, 1, 1, 1], 
+    [-4, 1, 1.2, 0], 
+    [5, 1, 1, 1], 
+    [-4, 1, 1.2, 0], 
+    [7, 1, 1, 1], 
+    [-4, 1, 1.2, 0], 
+    [9, 1, 1, 1], 
+    [-4, 1, 1.2, 0], 
+    [11, 1, 1, 1], 
+    [-4, 1, 1.2, 0],
+    [11, 3, 1, 1], 
+    [-4, 1, 1.2, 0],
     [11, 5, 1, 1],
-    [11, 7, 1, 1],
+    [-4, 1, 1.2, 0],
+    [11, 7, 1,1],
+    [-4, 1, 1.2, 0],
     [11, 9, 1, 1],
-    [11, 11, 1, 1],
-    [9, 11, 1, 1], #Top Side of box
-    [7, 11, 1, 1],
-    [5, 11, 1, 1],
-    [3, 11, 1, 1],
-    [1, 11, 1, 1], #Left side of box
+    [-4, 1, 1.2, 0],
+    [9, 9, 1, 1],
+    [-4, 1, 1.2, 0],
+    [7, 9, 1, 1],
+    [-4, 1, 1.2, 0],
+    [5, 9, 1, 1],
+    [-4, 1, 1.2, 0],
+    [3, 9, 1, 1],
+    [-4, 1, 1.2, 0],
     [1, 9, 1, 1],
+    [-4, 1, 1.2, 0],
     [1, 7, 1, 1],
-    [1, 5, 1, 1]
+    [-4, 1, 1.2, 0],
+    [1, 5, 1, 1],
+    [-4, 1, 1.2, 0],
+    [1, 3, 1, 1]
 ]
 
 app = QtGui.QApplication([])
@@ -62,9 +80,9 @@ dock_two.addWidget(preview_widget)
 animated_preview_widget.setBackgroundColor(mkColor(155, 155, 155, 0))
 preview_widget.setBackgroundColor(mkColor(155, 155, 155, 0))
 
-grid = gl.GLGridItem(size=QtGui.QVector3D(12, 13, 1))
+grid = gl.GLGridItem(size=QtGui.QVector3D(8, 9, 1))
 grid.setSpacing(spacing=QtGui.QVector3D(0.5, 0.5, 0.5))
-grid.translate(6.5, 7, 0)
+grid.translate(4.5, 5, 0)
 animated_preview_widget.addItem(grid)
 preview_widget.addItem(grid)
 
@@ -89,17 +107,29 @@ axis_color_line = np.array([
     [1, 0, 0, 1]
 ])
 
-x_line = gl.GLLinePlotItem(pos=x_verts_line, color=axis_color_line, width=2, antialias=True)
+x_line = gl.GLLinePlotItem(
+    pos=x_verts_line, color=axis_color_line, width=2, antialias=True)
 x_line.translate(0.5, 0.5, 0)
-y_line = gl.GLLinePlotItem(pos=y_verts_line, color=axis_color_line, width=2, antialias=True)
+y_line = gl.GLLinePlotItem(
+    pos=y_verts_line, color=axis_color_line, width=2, antialias=True)
 y_line.translate(0.5, 0.5, 0)
-z_line = gl.GLLinePlotItem(pos=z_verts_line, color=axis_color_line, width=2, antialias=True)
+z_line = gl.GLLinePlotItem(
+    pos=z_verts_line, color=axis_color_line, width=2, antialias=True)
 z_line.translate(0.5, 0.5, 0)
 animated_preview_widget.addItem(x_line)
 animated_preview_widget.addItem(y_line)
 animated_preview_widget.addItem(z_line)
+preview_widget.addItem(x_line)
+preview_widget.addItem(y_line)
+preview_widget.addItem(z_line)
 
-def brick(x, y, z, blocktype):
+extruder = gl.GLBoxItem(size=QtGui.QVector3D(
+    0.5, 0.5, 8), color=(55, 155, 55, 255))
+extruder.translate(0.25, 0.25, 0)
+animated_preview_widget.addItem(extruder)
+
+def brick(x, y, z, blocktype, screen):
+    global extruder
     """Add a brick to the scene
 
     Arguments:
@@ -111,6 +141,10 @@ def brick(x, y, z, blocktype):
             1 -- red
             2 -- blue
             3 -- yellow
+        screen {int} -- Screen
+            0 -- both
+            1 -- preview
+            2 -- animated
     """
     verts_box = np.array([
         [0, 0, 0],
@@ -121,7 +155,7 @@ def brick(x, y, z, blocktype):
         [1, 0, 1],
         [1, 1, 1],
         [0, 1, 1]],
-                         dtype=float)
+        dtype=float)
 
     faces_box = np.array([
         [0, 1, 2],
@@ -139,76 +173,135 @@ def brick(x, y, z, blocktype):
     ])
 
     color_list = ()
-    if blocktype == 1:
+    no_skip_block = True
+    if blocktype == 0:
+        no_skip_block = False
+    elif blocktype == 1:
         color_list = (1, 0, 0, 0.8)
     elif blocktype == 2:
         color_list = (0, 0, 1, 0.8)
     elif blocktype == 3:
         color_list = (1, 1, 0, 0.8)
 
-    # probably not needed
-    #colors_box = np.array([
-    #    [1, 1, 1, 0.5],
-    #    [1, 1, 1, 0.5],
-    #    [1, 1, 1, 0.5],
-    #    [1, 1, 1, 0.5],
-    #    [1, 1, 1, 0.5],
-    #    [1, 1, 1, 0.5],
-    #    [1, 1, 1, 0.5],
-    #    [1, 1, 1, 0.5],
-    #    [1, 1, 1, 0.5],
-    #    [1, 1, 1, 0.5],
-    #    [1, 1, 1, 0.5],
-    #    [1, 1, 1, 0.5],
-    #])
-    # faceColors=colors_box
-    box = gl.GLMeshItem(vertexes=verts_box, faces=faces_box,
-                        color=color_list, glOptions='opaque', smooth=True)
-    frame = gl.GLBoxItem(color=(0, 0, 0, 255))
-    box.translate(x, y, z)
-    frame.translate(x, y, z)
-    animated_preview_widget.addItem(box)
-    preview_widget.addItem(box)
-    animated_preview_widget.addItem(frame)
-    preview_widget.addItem(frame)
+    if no_skip_block:
+        box = gl.GLMeshItem(vertexes=verts_box, faces=faces_box,
+                            color=color_list, glOptions='translucent', smooth=True)
+        frame = gl.GLBoxItem(color=(0, 0, 0, 255))
+        box.translate(x, y, z)
+        frame.translate(x, y, z)
 
-extruder = gl.GLBoxItem(size=QtGui.QVector3D(0.5, 0.5, 8), color=(55, 155, 55, 255))
-extruder.translate(1.25, 1.25, 0)
-animated_preview_widget.addItem(extruder)
+    if screen == 0:
+        if no_skip_block:
+            animated_preview_widget.addItem(box)
+            preview_widget.addItem(box)
+            animated_preview_widget.addItem(frame)
+            preview_widget.addItem(frame)
+
+        if extruder in animated_preview_widget.items:
+            animated_preview_widget.removeItem(extruder)
+        extruder = gl.GLBoxItem(size=QtGui.QVector3D(
+                                0.5, 0.5, 8), color=(55, 155, 55, 255))
+        extruder.translate(x+0.25, y+0.25, z+1)
+        animated_preview_widget.addItem(extruder)
+    elif screen == 1:
+        if no_skip_block:
+            preview_widget.addItem(box)
+            preview_widget.addItem(frame)
+        else:
+            pass
+    elif screen == 2:
+        if no_skip_block:
+            animated_preview_widget.addItem(box)
+            animated_preview_widget.addItem(frame)
+
+        if extruder in animated_preview_widget.items:
+            animated_preview_widget.removeItem(extruder)
+        extruder = gl.GLBoxItem(size=QtGui.QVector3D(
+                                0.5, 0.5, 8), color=(55, 155, 55, 255))
+        extruder.translate(x+0.25, y+0.25, z+1)
+        animated_preview_widget.addItem(extruder)
+
+
+
+
+
+
+brick(-2, 0.5, 0.2, 1, 0)
+brick(-2, 3.5, 0.2, 2, 0)
+brick(-2, 6.5, 0.2, 3, 0)
+
 for pos in model:
-    brick(pos[0]/2, pos[1]/2, pos[2]-1, pos[3])
+    brick(pos[0]/2, pos[1]/2, pos[2]-1, pos[3], 1)
 window.show()
 
-index = 0
+index = 1
 rotation = 0
-updown = True
-def update():
-    global index, updown, rotation
-    if index >= 1:
-        updown = False
-    elif index <= 0:
-        updown = True
-    
+
+current_x, current_y, current_z = 0, 0, 0
+def update_animated():
+    global index, model, current_x, current_y, current_z, grid
+    if index == len(model):
+        index = 1
+        for item in animated_preview_widget.items:
+            animated_preview_widget.items.remove(item)
+            item._setView(None)
+        
+        animated_preview_widget.items = []
+        animated_preview_widget.update()
+        brick(-2, 0.5, 0.2, 1, 2)
+        brick(-2, 3.5, 0.2, 2, 2)
+        brick(-2, 6.5, 0.2, 3, 2)
+        animated_preview_widget.addItem(grid)
+
+
+    #if index == 1:
+    #    current_x = 0
+    #    current_y = 0
+    #    current_z = 0
+    #else:
+    #    current_x = model[index-1][0]/2
+    #    current_y = model[index-1][1]/2 
+    #    current_z = model[index-1][2]/2
+
+    #x_dif = model[index][0]/2 - current_x/2
+
+    #y_dif = model[index][1]/2 - current_y/2
+
+    #z_dif = model[index][2]/2 - current_z/2
+
+    #extruder.translate((x_dif)+0.75, (y_dif)+0.75, z_dif)
+    #extruder["position"] = QtGui.QVector3D((model[index][0]/2)+0.5, (model[index][1]/2)+0.5, model[index][2])
+    if model[index][3] == 0:
+        brick(model[index][0]/2, model[index][1]/2, model[index][2]-1, 0, 2)
+    elif model[index][3] == 1:
+        brick(model[index][0]/2, model[index][1]/2, model[index][2]-1, 1, 2)
+    elif model[index][3] == 2:
+        brick(model[index][0]/2, model[index][1]/2, model[index][2]-1, 2, 2)
+    elif model[index][3] == 3:
+        brick(model[index][0]/2, model[index][1]/2, model[index][2]-1, 3, 2)
+
+    index += 1
+
+gl
+def update_preview():
+    global rotation
     if rotation >= 360:
         rotation = 0
-    
 
     preview_widget.opts['center'] = QtGui.QVector3D(6.5, 6.5, 0)
     preview_widget.setCameraPosition(elevation=25, azimuth=rotation)
-    rotation += 0.5
+    rotation += 1
 
-    if updown:
-        extruder.translate(0, 0, 0.1)
-        index += 0.1
-    else:
-        extruder.translate(0, 0, -0.1)
-        index -= 0.1
 
-timer = QtCore.QTimer()
-timer.timeout.connect(update)
-timer.start(30)
+preview_timer = QtCore.QTimer()
+preview_timer.timeout.connect(update_preview)
+preview_timer.start(100)
 
-#timer.setInterval(100)
+animated_timer = QtCore.QTimer()
+animated_timer.timeout.connect(update_animated)
+animated_timer.start(300)
+
+# timer.setInterval(100)
 if __name__ == '__main__':
     import sys
     if (sys.flags.interactive != 1) or not hasattr(QtCore, 'PYQT_VERSION'):
