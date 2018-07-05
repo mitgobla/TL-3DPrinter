33e649d0e3bf134c315dfb94371576c6185e3818
mitgobla <ben-dodd@outlook.com>
2018-07-05 20:59:51 +0100

    render-test now renders coordinates from create-coordinates.

--- lego-print/resources/render-test.py
+++ lego-print/resources/render-test.py
@@ -12,6 +12,29 @@ from pyqtgraph.dockarea import *
 from pyqtgraph import mkColor
 from pyqtgraph.Qt import QtCore, QtGui
 
+script_dir = os.path.dirname(os.path.realpath(__file__))
+
+model = [
+    [1, 3, 1, 1], #Front of Box
+    [3, 3, 1, 1],
+    [5, 3 , 1, 1],
+    [7, 3 , 1 , 1],
+    [9, 3 , 1, 1],
+    [11, 3, 1, 1],  #Right Side of box
+    [11, 5, 1, 1],
+    [11, 7, 1, 1],
+    [11, 9, 1, 1],
+    [11, 11, 1, 1],
+    [9, 11, 1, 1], #Top Side of box
+    [7, 11, 1, 1],
+    [5, 11, 1, 1],
+    [3, 11, 1, 1],
+    [1, 11, 1, 1], #Left side of box
+    [1, 9, 1, 1],
+    [1, 7, 1, 1],
+    [1, 5, 1, 1]
+]
+
 app = QtGui.QApplication([])
 window = QtGui.QMainWindow()
 area = DockArea()
@@ -20,21 +43,61 @@ window.setCentralWidget(area)
 window.resize(1000, 500)
 window.setWindowTitle('3D Printing Preview')
 
-dock_one = Dock("Printing Preview", size=(500, 300))
+dock_one = Dock("Animated Preview", size=(500, 300))
+dock_two = Dock("Finished Preview", size=(500, 300))
 area.addDock(dock_one, 'left')
+area.addDock(dock_two, 'right')
 
-widget = gl.GLViewWidget()
-widget.opts['distance'] = 20
-widget.show()
-widget.setWindowTitle('Lil Timmy 3D Printing Preview')
-dock_one.addWidget(widget)
-widget.setBackgroundColor(mkColor(155, 155, 155, 0))
-script_dir = os.path.dirname(os.path.realpath(__file__))
+animated_preview_widget = gl.GLViewWidget()
+animated_preview_widget.opts['distance'] = 20
+animated_preview_widget.show()
+preview_widget = gl.GLViewWidget()
+preview_widget.opts['distance'] = 20
+preview_widget.show()
+
+
+dock_one.addWidget(animated_preview_widget)
+dock_two.addWidget(preview_widget)
+
+animated_preview_widget.setBackgroundColor(mkColor(155, 155, 155, 0))
+preview_widget.setBackgroundColor(mkColor(155, 155, 155, 0))
 
 grid = gl.GLGridItem(size=QtGui.QVector3D(12, 13, 1))
-grid.translate(6, 6.5, 0)
-widget.addItem(grid)
+grid.setSpacing(spacing=QtGui.QVector3D(0.5, 0.5, 0.5))
+grid.translate(6.5, 7, 0)
+animated_preview_widget.addItem(grid)
+preview_widget.addItem(grid)
 
+
+x_verts_line = np.array([
+    [0, 0, 0],
+    [2, 0, 0]
+])
+
+y_verts_line = np.array([
+    [0, 0, 0],
+    [0, 2, 0]
+])
+
+z_verts_line = np.array([
+    [0, 0, 0],
+    [0, 0, 2]
+])
+
+axis_color_line = np.array([
+    [1, 0, 0, 1],
+    [1, 0, 0, 1]
+])
+
+x_line = gl.GLLinePlotItem(pos=x_verts_line, color=axis_color_line, width=2, antialias=True)
+x_line.translate(0.5, 0.5, 0)
+y_line = gl.GLLinePlotItem(pos=y_verts_line, color=axis_color_line, width=2, antialias=True)
+y_line.translate(0.5, 0.5, 0)
+z_line = gl.GLLinePlotItem(pos=z_verts_line, color=axis_color_line, width=2, antialias=True)
+z_line.translate(0.5, 0.5, 0)
+animated_preview_widget.addItem(x_line)
+animated_preview_widget.addItem(y_line)
+animated_preview_widget.addItem(z_line)
 
 def brick(x, y, z, blocktype):
     """Add a brick to the scene
@@ -104,25 +167,36 @@ def brick(x, y, z, blocktype):
     frame = gl.GLBoxItem(color=(0, 0, 0, 255))
     box.translate(x, y, z)
     frame.translate(x, y, z)
-    widget.addItem(box)
-    widget.addItem(frame)
+    animated_preview_widget.addItem(box)
+    preview_widget.addItem(box)
+    animated_preview_widget.addItem(frame)
+    preview_widget.addItem(frame)
 
 extruder = gl.GLBoxItem(size=QtGui.QVector3D(0.5, 0.5, 8), color=(55, 155, 55, 255))
 extruder.translate(1.25, 1.25, 0)
-widget.addItem(extruder)
-for blocks in range(4):
-    brick(blocks, 0, 0, 3)
+animated_preview_widget.addItem(extruder)
+for pos in model:
+    brick(pos[0]/2, pos[1]/2, pos[2]-1, pos[3])
 window.show()
 
 index = 0
+rotation = 0
 updown = True
 def update():
-    global index, updown
+    global index, updown, rotation
     if index >= 1:
         updown = False
     elif index <= 0:
         updown = True
     
+    if rotation >= 360:
+        rotation = 0
+    
+
+    preview_widget.opts['center'] = QtGui.QVector3D(6.5, 6.5, 0)
+    preview_widget.setCameraPosition(elevation=25, azimuth=rotation)
+    rotation += 0.5
+
     if updown:
         extruder.translate(0, 0, 0.1)
         index += 0.1
@@ -134,7 +208,7 @@ timer = QtCore.QTimer()
 timer.timeout.connect(update)
 timer.start(30)
 
-timer.setInterval(100)
+#timer.setInterval(100)
 if __name__ == '__main__':
     import sys
     if (sys.flags.interactive != 1) or not hasattr(QtCore, 'PYQT_VERSION'):
