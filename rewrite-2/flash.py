83163d465c0286baa050488c82e00a757aad8f24
mitgobla <ben-dodd@outlook.com>
2018-07-23 18:01:32 +0100

    Initial Update Commit

--- rewrite-2/flash.py
+++ rewrite-2/flash.py
@@ -1,11 +1,105 @@
+import ctypes
 import sys
+import os
+import tkinter as tk
+from tkinter import filedialog, messagebox
+from ast import literal_eval
+
 from PyQt5.QtWidgets import QApplication, QMainWindow
 from main_window import Ui_MainWindow
+import generator
 
-app = QApplication(sys.argv)
-window = QMainWindow()
-ui = Ui_MainWindow()
-ui.setupUi(window)
+SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
+APP_ID = "mitgobla.teamlightning.flash.alpha"
+ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(APP_ID)
 
-window.show()
-sys.exit(app.exec_())
+class FileLoader:
+    """Loads a text file of coordinates and colours
+    """
+
+    def __init__(self):
+        self.window = tk.Tk()
+        self.window.withdraw()
+        self.window.wm_iconbitmap(SCRIPT_DIR+"\\bolt-icon.ico")
+        self.file = None
+        self.previous = None
+
+    def load_txt(self):
+        """Load the list of brick positions and colours
+        """
+        self.previous = self.file
+        self.window.filename = filedialog.askopenfilename(
+            initialdir=SCRIPT_DIR,
+            title="Select Coordinate file",
+            filetypes=[("Text Files", "*.txt")])
+        self.file = self.window.filename
+        if not self.file:
+            self.file = self.previous
+            return False
+        return True
+
+    def load_flash(self):
+        """Load a FLASH file
+        """
+        self.previous = self.file
+        self.window.filename = filedialog.askopenfilename(
+            initialdir=SCRIPT_DIR,
+            title="Select FLASH file",
+            filetypes=[("FLASH Files", "*.flash")])
+        self.file = self.window.filename
+        if not self.file:
+            self.file = self.previous
+            return False
+        return True
+
+    @property
+    def return_coordinates(self):
+        """Convert FLASH to coordinates
+
+        Returns:
+            list -- Array of Lists [x, y, z, c]
+        """
+
+        return literal_eval(self.file)
+
+
+
+
+class FileGenerator:
+
+    def __init__(self):
+        self.loader = FileLoader()
+
+    def generate_flash_file(self):
+        if self.loader.load_txt(): # Continue if a file was selected
+            model = generator.Model(self.loader.file)
+            if model.read(): # Continue if a valid model file
+                generate = generator.Generate(model)
+                generate.gen_model()
+                generate.write_model()
+                messagebox.showinfo("Completed", "FLASH file generated as model.flash")
+            else:
+                messagebox.showerror("Error", "Invalid model file.")
+
+    def load_flash_file(self):
+        if self.loader.load_flash():
+            model_content = open(self.loader.file, "r")
+            model = literal_eval(model_content.read())
+            model_content.close()
+            #PREVIEWER.model = model
+            #PREVIEWER.generate_model(0)
+            #PREVIEWER.generate_model(1)
+
+
+LOADER = FileGenerator()
+
+APP = QApplication(sys.argv)
+WINDOW = QMainWindow()
+UI = Ui_MainWindow()
+UI.setupUi(WINDOW)
+
+UI.actionOpen.triggered.connect(LOADER.load_flash_file)
+UI.actionConvert.triggered.connect(LOADER.generate_flash_file)
+
+WINDOW.show()
+sys.exit(APP.exec_())
---
+++ rewrite-2/generator.py
@@ -0,0 +1,120 @@
+"""
+Title: FLASH Model Generator
+Description: Converts coordinates into FLASH code.
+
+Author: Ben Dodd (mitgobla)
+Email: ben-dodd@outlook.com
+Website: http://www.team-lightning.ga
+
+
+Each position has 4 values:
+    x: the x coordinate
+    y: the y coordinate
+    z: the z coordinate
+    c: the block colour
+    c = 0 - 3; 0 being none
+        1: red
+        2: blue
+        3: yellow
+"""
+
+import os
+SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
+
+class Model:
+    """Convert file to FLASH model format
+
+    Arguments:
+        path {file} -- File of model coordinates and colours
+    """
+    def __init__(self, path):
+        """Convert file to FLASH model format
+
+        Arguments:
+            path {file} -- File of model coordinates and colours
+        """
+        self.file = None
+        self.file_list = []
+        self.coordinates = []
+        self.model_path = path
+
+    def read(self):
+        """Read the given file.
+        """
+        #Open the given file
+        self.file = open(self.model_path, "r")
+        #Assuming each coordinate is per line
+        self.file_list = self.file.read().split('\n')
+        self.file.close()
+        #Loop through every line
+        if not self.file_list[0].startswith('#'):
+            return False
+        for line in self.file_list:
+            #Ignore comments in the model
+            if not line.startswith('#'):
+                next_coords = line.split(', ')
+                if len(next_coords) != 4:
+                    return False
+                for pos in range(len(next_coords)):
+                    next_coords[pos] = float(next_coords[pos])
+                self.coordinates.append(next_coords)
+        return True
+
+    @property
+    def get_model(self):
+        """Return the model coordinates
+        """
+        return self.coordinates
+
+
+
+class Generate:
+    """Generate print instructions from model coordinates
+
+    Arguments:
+        model {generator.Model} -- Model object
+    """
+
+    def __init__(self, model):
+        """Generate print instructions from model coordinates
+
+        Arguments:
+            model {generator.Model} -- Model object
+        """
+
+        self.model_coordinates = []
+        self.coordinates = []
+        self.model = model
+
+    def gen_model(self):
+        """Generate the FLASH model
+        """
+        self.model_coordinates = self.model.get_model
+
+        self.coordinates = []
+        for pos in range(len(self.model_coordinates)):
+            if self.model_coordinates[pos][3] == 1:
+                self.coordinates.append([-4, 1, 1.2, 0]) #Red Brick Coord
+            elif self.model_coordinates[pos][3] == 2:
+                self.coordinates.append([-4, 7, 1.2, 0]) #Blue Brick Coord
+            elif self.model_coordinates[pos][3] == 3:
+                self.coordinates.append([-4, 13, 1.2, 0]) #Yellow Brick Coord
+            self.coordinates.append(self.model_coordinates[pos])
+
+    def write_model(self):
+        """Write the FLASH file and the EV3 .rtf file
+        """
+        if not os.path.exists(SCRIPT_DIR+'\\models'):
+            os.makedirs(SCRIPT_DIR+'\\models')
+
+        flash_file = open(SCRIPT_DIR+'\\models\\model.flash', 'w')
+        flash_file.write(str(self.coordinates))
+        flash_file.close()
+
+        model_file = open(SCRIPT_DIR+'\\models\\model.rtf', 'w')
+        model_file.write("{:.4f}".format(len(self.coordinates))+chr(13))
+        for axes in range(4):
+            for pos in self.coordinates:
+                #write each number on a new line
+                model_file.write("{:.4f}".format(pos[axes])+chr(13))
+        model_file.close()
